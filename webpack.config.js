e8c6a79ed8b130a0260b25bc45e19bdcf1dfc8d1
Max <mhmad.hamster@gmail.com>
2015-12-03 01:33:18 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+}
